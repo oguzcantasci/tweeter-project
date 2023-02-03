ba5224048e2a2c8927cc26f9bc52428c6ae3cf5d
Ozzy Tasci <oguzcantasci@gmail.com>
2023-02-03 10:49:38 -0500

    Add comments for better readability

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,14 +10,15 @@
 
 $(function() {
 
+  // Escape function for XSS
   const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
+  // Function to create tweet in HTML
   const createTweetElement = function(tweet) {
-
     const $tweet = $(`<article class="tweet">
     <header>
       <div>
@@ -39,6 +40,7 @@ $(function() {
     return $tweet;
   };
 
+  //Function to append a created tweet to HTML
   const renderTweets = function(tweetArray) {
     tweetArray.reverse();
     $('#tweets-container').empty();
@@ -48,18 +50,23 @@ $(function() {
     }
   };
 
+  // Function to show tweets on the webpage
   const loadTweets = function() {
     $.get("/tweets", function(tweets) {
       renderTweets(tweets);
     });
   };
 
+  // Hide errors on page load to reveal them later if necessary
   $(".error-container").hide();
   
+  // Load tweets from the tweets database
   loadTweets();
 
+  //  Logic to handle new tweet submission
   $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
+    // Validation checks
     if (!$("#tweet-text").val().trim()) {
       $(".error-text").text("Empty tweet is a bad tweet");
       $(".error-container").slideDown("slow");
@@ -71,12 +78,14 @@ $(function() {
       return;
     }
     const formData = $(this).serialize();
+    //
     $.post("/tweets", formData, function() {
-      loadTweets();
-      $("#tweet-text").val("");
-      $(".counter").text(140);
-      $(".error-text").text("");
-      $(".error-container").slideUp();
+      // On successful submission
+      loadTweets(); // Load tweets again
+      $("#tweet-text").val(""); // Reset form
+      $(".counter").text(140); // Reset character counter
+      $(".error-text").text(""); // Reset error
+      $(".error-container").slideUp(); // Hide error
     });
   });
 });
